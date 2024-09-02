5ddead27304939ac05e05bbc31d17e73441f34ab
emmy-studios <emmystudios@proton.me>
2024-09-01 21:56:44 -0600

    "Create basic project structure"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome | Restaurant Booking System')
+
+@section('content')
+    
+    <main>
+        <div class="flex flex-row justify-center items-center">
+            <h1 class="text-red-500 font-bold">Home Page</h1>
+        </div>
+
+        <div class="flex flex-row my-10">
+            <x-dropdown text="Menu" position="bottom-end">
+                <x-dropdown.items text="Profile" />
+                <x-dropdown.items text="Dashboard" separator />
+            </x-dropdown>
+        </div>
+
+    </main>
+
+@endsection
