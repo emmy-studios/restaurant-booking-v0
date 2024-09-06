c5331369cca8b6cebee563dda5fb5a2b0029d95c
emmy-studios <emmystudios@proton.me>
2024-09-05 22:52:34 -0600

    "Create users models part-1"

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,29 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    const ADMIN_ROLE = 'ADMIN';
+    const SUPERVISOR_ROLE = 'SUPERVISOR';
+    const CUSTOMER_ROLE = 'CUSTOMER';
+    const EMPLOYEE_ROLE = 'EMPLOYEE';
+
+    const ROLES = [
+        self::ADMIN_ROLE => 'admin',
+        self::SUPERVISOR_ROLE => 'supervisor',
+        self::CUSTOMER_ROLE => 'customer',
+        self::EMPLOYEE_ROLE => 'employee',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +34,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'first_name',
+        'last_name',
+        'country_code',
+        'phone_number',
+        'country',
+        'city',
+        'address',
         'email',
+        'role',
         'password',
     ];
 
@@ -42,4 +65,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin() || $this->isSupervisor() || $this->isEmployee();
+    }
+
+    public function isAdmin(){
+        return $this->role === self::ADMIN_ROLE;
+    }
+
+    public function isSupervisor(){
+        return $this->role === self::SUPERVISOR_ROLE;
+    }
+
+    public function isEmployee(){
+        return $this->role === self::EMPLOYEE_ROLE;
+    }
 }
+
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,24 +1,34 @@
 <?php
 
+use App\Enums\CountryCode;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\text;
+
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. 
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
+            $table->enum('country_code', array_map(fn($code) => $code->value, CountryCode::cases()))->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->text('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
