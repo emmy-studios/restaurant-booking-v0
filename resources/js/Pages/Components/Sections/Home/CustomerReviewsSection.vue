d671f8e8e40c5d6db73c63b2f4ed873bdc1b5eaa
emmy-studios <emmystudios@proton.me>
2024-12-04 02:54:11 -0600

    Add homepage styles part-2

--- resources/js/Pages/Components/Sections/Home/CustomerReviewsSection.vue
+++ resources/js/Pages/Components/Sections/Home/CustomerReviewsSection.vue
@@ -3,11 +3,194 @@
 </script>
 
 <template>
-    <div>
-        <h1>Customer Reviews</h1>
+
+    <div class="review-container">
+
+        <div class="header-container">
+            <h2>Customer Review</h2>
+            <hr>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec viverra mattis neque at aliquam. Morbi elementum sit
+                amet risus varius ultricies. Ut feugiat erat vitae magna
+                dignissim, a mollis urna finibus.
+            </p>
+        </div>
+
+        <div class="review-cards">
+
+            <div class="review-card">
+                <div class="review-text">
+                    <p>
+                        Donec viverra mattis neque at aliquam. Morbi elementum sit
+                        amet risus varius ultricies. Ut feugiat erat vitae magna
+                        dignissim, a mollis urna finibus.
+                    </p>
+                </div>
+                <div class="customer-review">
+                    <div class="customer-info">
+                        <img src="assets/images/panels/chef_profile.png">
+                        <div class="customer-text">
+                            <span>Jon Doe</span>
+                            <p>customer</p>
+                        </div>
+                    </div>
+                    <div class="text-decoration">
+                        <span>;;</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="review-card">
+                <div class="review-text">
+                    <p>
+                        Donec viverra mattis neque at aliquam. Morbi elementum sit
+                        amet risus varius ultricies. Ut feugiat erat vitae magna
+                        dignissim, a mollis urna finibus.
+                    </p>
+                </div>
+                <div class="customer-review">
+                    <div class="customer-info">
+                        <img src="assets/images/panels/chef_profile.png">
+                        <div class="customer-text">
+                            <span>Jon Doe</span>
+                            <p>customer</p>
+                        </div>
+                    </div>
+                    <div class="text-decoration">
+                        <span>;;</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="review-card">
+                <div class="review-text">
+                    <p>
+                        Donec viverra mattis neque at aliquam. Morbi elementum sit
+                        amet risus varius ultricies. Ut feugiat erat vitae magna
+                        dignissim, a mollis urna finibus.
+                    </p>
+                </div>
+                <div class="customer-review">
+                    <div class="customer-info">
+                        <img src="assets/images/panels/chef_profile.png">
+                        <div class="customer-text">
+                            <span>Jon Doe</span>
+                            <p>customer</p>
+                        </div>
+                    </div>
+                    <div class="text-decoration">
+                        <span>;;</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
+
 </template>
 
 <style scoped>
+
+    .review-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding-top: 20px;
+        background-color: #202020;
+    }
+    .header-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        padding-top: 50px;
+        padding-bottom: 30px;
+    }
+    .header-container h2 {
+        font-weight: bold;
+        font-size: 30px;
+        color: #fff;
+        text-transform: uppercase;
+    }
+    .header-container hr {
+        border: 1px solid orange;
+        height: 1px;
+        width: 15%;
+    }
+    .header-container p {
+        color: #7f9183;
+        max-width: 50%;
+        text-align: center;
+        padding-top: 10px;
+    }
+    .review-cards {
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+        padding-left: 30px;
+        padding-right: 30px;
+        padding-bottom: 100px;
+    }
+    .review-card {
+        background-color: #000;
+        border-radius: 10px;
+    }
+    .review-text {
+        display: flex;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+    .review-text p {
+        color: #fff;
+    }
+    .customer-review {
+        display: flex;
+        justify-content: space-between;
+        padding-left: 20px;
+        padding-right: 10px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    .customer-info {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+    }
+    .customer-info img {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+    }
+    .customer-text {
+        display: flex;
+        flex-direction: column;
+        gap: 2px;
+    }
+    .customer-text span{
+        color: #fff;
+        font-weight: bold;
+        font-size: 16px;
+    }
+    .customer-text p {
+        color: #fff;
+    }
+    .text-decoration span {
+        color: orange;
+        font-size: 60px;
+        font-weight: italic;
+    }
+    @media (max-width: 902px) {
+        .review-cards {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+    }
 
 </style>
--- resources/js/Pages/Components/Sections/Home/SubscribeSection.vue
+++ resources/js/Pages/Components/Sections/Home/SubscribeSection.vue
@@ -1,5 +1,47 @@
+<script setup>
+
+    import { NButton } from 'naive-ui';
+
+</script>
+
 <template>
-    <div>
-        <h1>Subscribe Us</h1>
+
+    <div class="subscribe-container">
+        <div class="subscribe-title">
+            <h2>Get Exclusive Update</h2>
+            <p>
+                Lorem ipsum dolerum notre damun.
+            </p>
+        </div>
+        <div class="action-buttons">
+            <input/>
+            <n-button type="primary">Subscribe Now</n-button>
+        </div>
     </div>
+
 </template>
+
+<style scoped>
+
+    .subscribe-container {
+        height: 30vh;
+        background-color: orange;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .subscribe-title h2 {
+        font-weight: bold;
+        text-transform: uppercase;
+        color: #000;
+        font-size: 30px;
+    }
+    .subscribe-title p {
+        color: #fff;
+    }
+    .action-buttons {
+        display: flex;
+        gap: 10px;
+    }
+
+</style>
